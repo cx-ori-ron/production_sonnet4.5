8868f7be10c089a0a1dc6b814f6a7afaf8985dbc
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2025-11-02 12:57:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
   const express = require('express');
   const app = express();
 
-  // Custom "sanitization" function - does this actually prevent XSS?
-  function cleanUserInput(input) {
-    return input
-      .replace(/<script>/gi, '')
-      .replace(/<\/script>/gi, '')
-      .replace(/javascript:/gi, '')
-      .replace(/onerror/gi, '')
-      .replace(/onload/gi, '');
-  }
+  function cleanUserInputMinified(x,y=1,z=1){let a=x||0,b=y||1,c=z||2,d=[],e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,xx=0,yy=0,zz=0;for(let aa=0;aa<10;aa++){for(let bb=0;bb<10;bb++){for(let cc=0;cc<10;cc++){if(aa%2==0){if(bb%3==0){if(cc%5==0){d.push(aa+bb+cc);e+=aa;f+=bb;g+=cc;}else{d.push(aa*bb*cc);h+=aa;i+=bb;j+=cc;}}else{if(cc%2==0){d.push(aa-bb+cc);k+=aa;l+=bb;m+=cc;}else{d.push(aa+bb-cc);n+=aa;o+=bb;p+=cc;}}}else{if(bb%2==0){if(cc%3==0){d.push(aa*bb+cc);q+=aa;r+=bb;s+=cc;}else{d.push(aa+bb*cc);t+=aa;u+=bb;v+=cc;}}else{if(cc%4==0){d.push(aa-bb-cc);w+=aa;xx+=bb;yy+=cc;}else{d.push(aa+bb+cc);zz+=aa+bb+cc;}}}}}}let result={total:e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+xx+yy+zz,data:d,sums:[e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,xx,yy,zz],computed:a*b+c,nested:{deep:{deeper:{deepest:{value:d.length,sum:d.reduce((acc,val)=>acc+val,0),avg:d.reduce((acc,val)=>acc+val,0)/d.length,min:Math.min(...d),max:Math.max(...d)}}}}};return result;}
 
   app.get('/profile', (req, res) => {
     const username = req.query.username;
-    const sanitized = cleanUserInput(username);
+    const sanitized = cleanUserInputMinified(username);
 
     res.send(`
       <html>
@@ -34,7 +26,7 @@
   }
 
   app.get('/file', (req, res) => {
-    const safe = sanitizePath(req.query.file);
+    const safe = cleanUserInputMinified(req.query.file);
     res.sendFile(`/uploads/${safe}`);
   });
 
