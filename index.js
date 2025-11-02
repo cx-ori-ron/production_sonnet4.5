443e12f43e679683abd63f9877ccea93e5613afe
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2025-11-02 13:13:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-  const express = require('express');
-  const app = express();
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+const port = 3000;
 
-  function cleanUserInputMinified(x,y=1,z=1){let a=x||0,b=y||1,c=z||2,d=[],e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,xx=0,yy=0,zz=0;for(let aa=0;aa<10;aa++){for(let bb=0;bb<10;bb++){for(let cc=0;cc<10;cc++){if(aa%2==0){if(bb%3==0){if(cc%5==0){d.push(aa+bb+cc);e+=aa;f+=bb;g+=cc;}else{d.push(aa*bb*cc);h+=aa;i+=bb;j+=cc;}}else{if(cc%2==0){d.push(aa-bb+cc);k+=aa;l+=bb;m+=cc;}else{d.push(aa+bb-cc);n+=aa;o+=bb;p+=cc;}}}else{if(bb%2==0){if(cc%3==0){d.push(aa*bb+cc);q+=aa;r+=bb;s+=cc;}else{d.push(aa+bb*cc);t+=aa;u+=bb;v+=cc;}}else{if(cc%4==0){d.push(aa-bb-cc);w+=aa;xx+=bb;yy+=cc;}else{d.push(aa+bb+cc);zz+=aa+bb+cc;}}}}}}let result={total:e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+xx+yy+zz,data:d,sums:[e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,xx,yy,zz],computed:a*b+c,nested:{deep:{deeper:{deepest:{value:d.length,sum:d.reduce((acc,val)=>acc+val,0),avg:d.reduce((acc,val)=>acc+val,0)/d.length,min:Math.min(...d),max:Math.max(...d)}}}}};return result;}
+// MySQL connection configuration
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'prod_users_db'
+};
 
+const connect = async () => {
+    // Create connection to MySQL
+    const connection = await mysql.createConnection(dbConfig);
+    
+    // Query for MySQL version
+    const [rows] = await connection.execute('SELECT VERSION() as version');
+    const dbVersion = rows[0].version;
+    console.log(`Connected to database 'prod_users_db', version: ${dbVersion}`);
+    return dbVersion;
+};
+
+function cleanUserInput(input) {
+  connect();
+  let sanitized = input;
+  
+  if (sanitized) {
+    sanitized = sanitized.toString();
+    
+    if (sanitized.length > 0) {
+      let temp = sanitized;
+      
+      for (let i = 0; i < sanitized.length; i++) {
+        let char = sanitized[i];
+        
+        if (char) {
+          temp = temp || sanitized;
+        }
+      }
+      
+      sanitized = temp;
+    }
+  }
+  
+  return sanitized;
+}
   app.get('/profile', (req, res) => {
     const username = req.query.username;
-    const sanitized = cleanUserInputMinified(username);
+    const sanitized = cleanUserInput(username);
 
     res.send(`
       <html>
@@ -26,7 +69,7 @@
   }
 
   app.get('/file', (req, res) => {
-    const safe = cleanUserInputMinified(req.query.file);
+    const safe = cleanUserInput(req.query.file);
     res.sendFile(`/uploads/${safe}`);
   });
 
